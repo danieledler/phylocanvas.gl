8637c379f84090b2768880691c45458489dbe3ad
Khalil <ka10@sanger.ac.uk>
2021-10-30 16:11:42 +0100

    Update set-props.js

--- methods/set-props.js
+++ methods/set-props.js
@@ -19,7 +19,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-export default function setProps(updater) {
+export default function setProps(updater, eventOrigin) {
   if (updater) {
     this.props = Object.freeze({
       ...this.props,
